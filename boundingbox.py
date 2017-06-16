5ffb74192804c87ea98f02e29faa2185e8e5cb3a
Levi Buckwalter <levibuck4@gmail.com>
2017-06-15 23:08:06 -0400

    Initial add of boundingbox module

---
+++ boundingbox.py
@@ -0,0 +1,15 @@
+
+class BoundingBox:
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def fit_inside(self, enclosing):
+        h_pct = self.h / enclosing.h
+        w_pct = self.w / enclosing.w
+
+        scalar = h_pct if h_pct > w_pct else w_pct
+
+        self.w /= scalar
+        self.h /= scalar
