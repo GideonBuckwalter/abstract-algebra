b64226236d981852ccb52b5b06873b4f6c6153cf
Levi Buckwalter <levibuck4@gmail.com>
2017-06-23 21:32:52 -0400

    Abstract dimension getters out of Row/Column and MLoop/ALoop

--- loop.py
+++ loop.py
@@ -1,5 +1,41 @@
-class MLoop:
-    pass
+from row_column import Row, Column, Symbol
+import itertools
+import operator
 
-class ALoop:
-    pass
+class AbstractLoop:
+    PADDING = 5
+
+    _container = None
+    _primary_dimension = None
+    _secondary_dimension = None
+
+    def __init__(self, positive=None, negative=None):
+        self.positive = positive if positive is not None else self._container([])
+        self.negative = negative if negative is not None else self._container([])
+        self.rel_child_positions = []
+        
+        self.add_dim_getters(self._primary_dimension, operator.plus, 4)
+        self.add_dim_getters(self._secondary_dimension, max, 2)
+
+    def add_dim_getters(self, dim_name, func, pad_times):
+        setattr(self, "sym_" + dim_name,
+            lambda slf:
+                func(getattr(slf.positive, "sym_" + dim_name),
+                     getattr(slf.negative, "sym_" + dim_name)))
+
+        setattr(self, "pix_" + dim_name,
+            lambda slf:
+                self.PADDING * padd_times * \
+                func(getattr(slf.positive, "pix_" + dim_name),
+                     getattr(slf.negative, "pix_" + dim_name)))
+
+
+class MLoop(AbstractLoop):
+    _container = Column
+    _primary_dimension = "height"
+    _secondary_dimension = "width"
+
+class ALoop(AbstractLoop):
+    _container = Row
+    _primary_dimension = "width"
+    _secondary_dimension = "height"
--- row_column.py
+++ row_column.py
@@ -1,20 +1,58 @@
-class Row(list):
-    def sym_width(self):
-        return sum(ele.sym_width() for ele in self)
 
-    def sym_height(self):
-        return max(ele.sym_height() for ele in self)
+class ExprList(list):
+    
+    SPACING = 5
 
-class Column(list):
-    def sym_width(self):
-        return max(ele.sym_width() for ele in self)
+    _primary_dimension = None
+    _secondary_dimension = None
 
-    def sym_height(self):
-        return sum(ele.sym_height() for ele in self)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.add_dim_getter(self._primary_dimension, sum)
+        self.add_dim_getter(self._secondary_dimension, max)
+
+
+    def add_dim_getter(self, dim_name, func):
+        func_name = "sym_{}".format(dim_name)
+
+        setattr(self, func_name,
+            lambda slf:
+                func(getattr(ele, func_name)() for ele in slf))
+
+    def pix_height(self):
+        #      Symbol.PIX_HGT * self.sym_height() + self.SPACING * (self.sym_height() - 1)
+        return Symbol.PIX_HGT * (self.sym_height() + self.SPACING) - self.SPACING
+
+    def pix_width(self):
+        return Symbol.PIX_WTH * (self.sym_width() + self.SPACING) - self.SPACING
+
+
+
+
+class Row(ExprList):
+    _primary_dimension = "width"
+    _secondary_dimension = "height"
+
+class Column(ExprList):
+    _primary_dimension = "height"
+    _secondary_dimension = "width"
+
+
 
 class Symbol:
+
+    PIX_HGT = 50
+    PIX_WTH = 50
+
     def sym_width(self):
         return 1
 
     def sym_height(self):
         return 1
+
+    def pix_width(self):
+        return PIX_WTH
+
+    def pix_height(self):
+        return PIX_HGT
