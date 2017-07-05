9eab1bf1e2fcb2ab96449548012f999324d7afc6
Levi Buckwalter <levibuck4@gmail.com>
2017-07-05 11:48:11 -0400

    Implemented draw function

--- loop.py
+++ loop.py
@@ -12,7 +12,9 @@ class AbstractLoop:
     def __init__(self, positive=None, negative=None):
         self.positive = positive if positive is not None else self._container([])
         self.negative = negative if negative is not None else self._container([])
-        self.rel_child_positions = []
+
+        self.rel_snap_pos = Vector()
+        self.rel_phys_pos = Vector()
         
         self.add_dim_getters(self._primary_dimension, operator.plus, 4)
         self.add_dim_getters(self._secondary_dimension, max, 2)
@@ -28,14 +30,32 @@ class AbstractLoop:
                 self.PADDING * padd_times * \
                 func(getattr(slf.positive, "pix_" + dim_name),
                      getattr(slf.negative, "pix_" + dim_name)))
+    
+    def children(self):
+        return itertools.chain(self.positive, self.negative)
 
-    def draw(self, abs_pos):
-        self.render_loop(abs_pos)
+    def calc_child_rel_snap_positions(self):
+        # ASSUME THIS IS A ALOOP
 
-        vecs = self.rel_child_positions
-        kids = itertools.chain(self.positive, self.negative)
-        for vector, child in izip(vecs, kids):
-            child.draw(abs_pos + vector)
+        s = 0
+        for child in self.positive:
+            s += self.PADDING
+            s += child.pix_width() / 2
+            child.rel_snap_pos = Vector(s, 0)
+            s += child.pix_width() / 2
+
+        s = 0
+        for child in self.negative:
+            s += self.PADDING
+            s += child.pix_width() / 2
+            child.rel_snap_pos = Vector(-s, 0)
+            s += child.pix_width() / 2
+
+    def draw(self, parent_screen_pos):
+        screen_pos = parent_screen_pos + self.rel_snap_pos + self.rel_phys_pos
+        self.render_loop(screen_pos)
+        for child in self.children():
+            child.draw(screen_pos)
 
 
 class MLoop(AbstractLoop):
@@ -44,10 +64,27 @@ class MLoop(AbstractLoop):
     _secondary_dimension = "width"
 
     # METAPROGRAM AWAY IN FUTURE
-    def render_loop(self, abs_pos):
+    def render_loop(self, screen_pos):
         stroke(255, 255, 255)
         noFill()
-        rect(abs_pos.x, abs_pos.y, self.pix_width(), self.pix_height())
+        rect(screen_pos.x, screen_pos.y, self.pix_width(), self.pix_height())
+
+    def calc_child_rel_snap_positions(self):
+        # ASSUME THIS IS A ALOOP
+
+        s = 0
+        for child in self.positive:
+            s += self.PADDING
+            s += child.pix_height() / 2
+            child.rel_snap_pos = Vector(0, s)
+            s += child.pix_height() / 2
+
+        s = 0
+        for child in self.negative:
+            s += self.PADDING
+            s += child.pix_height() / 2
+            child.rel_snap_pos = Vector(0, -s)
+            s += child.pix_height() / 2
 
 class ALoop(AbstractLoop):
     _container = Row
@@ -55,10 +92,25 @@ class ALoop(AbstractLoop):
     _secondary_dimension = "height"
 
     # METAPROGRAM AWAY IN FUTURE
-    def render_loop(self, abs_pos):
+    def render_loop(self, screen_pos):
         stroke(255, 255, 255)
         noFill()
-        rect(abs_pos.x, abs_pos.y, self.pix_width(), self.pix_height())
+        rect(screen_pos.x, screen_pos.y, self.pix_width(), self.pix_height())
 
+    def calc_child_rel_snap_positions(self):
+        # ASSUME THIS IS A ALOOP
 
+        s = 0
+        for child in self.positive:
+            s += self.PADDING
+            s += child.pix_width() / 2
+            child.rel_snap_pos = Vector(s, 0)
+            s += child.pix_width() / 2
+
+        s = 0
+        for child in self.negative:
+            s += self.PADDING
+            s += child.pix_width() / 2
+            child.rel_snap_pos = Vector(-s, 0)
+            s += child.pix_width() / 2
 
